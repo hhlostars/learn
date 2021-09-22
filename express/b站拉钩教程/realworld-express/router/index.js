1327bd2f5dcc9bb8a4b88001a28f1640be5a8ce5
codehh <hh646371951@gmail.com>
2021-09-22 21:56:46 +0800

    9-22

--- express/b站拉钩教程/realworld-express/router/index.js
+++ express/b站拉钩教程/realworld-express/router/index.js
@@ -6,4 +6,36 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.get('/login', (req, res) => {
+  res.render('login', {
+    isLogin: true
+  })
+})
+
+router.get('/register', (req, res) => {
+  res.render('login', {
+    isLogin: true
+  })
+})
+
+router.get('/settings', (req, res) => {
+  res.render('settings')
+})
+
+router.get('/editor', (req, res) => {
+  res.render('editor')
+})
+
+router.get('/article/:articleId', (req, res) => {
+  res.render('article')
+})
+
+router.get('/profile/:username', (req, res) => {
+  res.render('profile')
+})
+
+router.get('/profile/:username/favorites', (req, res) => {
+  res.render('profile')
+})
+
 module.exports = router
