adb990d48330ecbfeac945ee859ddf8e978522c2
codehh <hh646371951@gmail.com>
2022-01-20 15:38:36 +0800

    111

---
+++ React/官方文档/练习.js
@@ -0,0 +1,90 @@
+// import { getImageUrl } from './utils.js';
+
+function Profile(person, size) {
+  const src = getImageUrl(person.)
+
+  return (
+    <section className="profile">
+      <h2>person.name</h2>
+      <img
+        className="avatar"
+        src={getImageUrl('szV5sdG')}
+        alt="Maria Skłodowska-Curie"
+        width={70}
+        height={70}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          physicist and chemist
+        </li>
+        <li>
+          <b>Awards: 4 </b>
+          (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
+        </li>
+        <li>
+          <b>Discovered: </b>
+          polonium (element)
+        </li>
+      </ul>
+    </section>
+  )
+}
+
+export default function Gallery() {
+  return (
+    <div>
+      <Profile />
+      <h1>Notable Scientists</h1>
+      <section className="profile">
+        {/* name, url, alt = name , width, height, profession, awards:[], discovered: [] */}
+        <h2>Maria Skłodowska-Curie</h2>
+        <img
+          className="avatar"
+          src={getImageUrl('szV5sdG')}
+          alt="Maria Skłodowska-Curie"
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            physicist and chemist
+          </li>
+          <li>
+            <b>Awards: 4 </b>
+            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
+          </li>
+          <li>
+            <b>Discovered: </b>
+            polonium (element)
+          </li>
+        </ul>
+      </section>
+      <section className="profile">
+        <h2>Katsuko Saruhashi</h2>
+        <img
+          className="avatar"
+          src={getImageUrl('YfeOqp2')}
+          alt="Katsuko Saruhashi"
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            geochemist
+          </li>
+          <li>
+            <b>Awards: 2 </b>
+            (Miyake Prize for geochemistry, Tanaka Prize)
+          </li>
+          <li>
+            <b>Discovered: </b>
+            a method for measuring carbon dioxide in seawater
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
